c0d1ab5ea6dfb577ed23efaf9e056c9d7aa7acd6
silviaespanagil <silviaespanagil@gmail.com>
2021-08-07 13:29:12 +0200

    Add render menu to header component and install router dom

--- src/components/0 - App.js
+++ src/components/0 - App.js
@@ -1,11 +1,17 @@
 //COMPONENTS
+import Header from "./1-Header";
 import Main from "./2-Main";
 
 //STYLESHEET
 import "../stylesheets/App.scss";
 
 function App() {
-  return <Main />;
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
 }
 
 export default App;
--- src/components/1-Header.js
+++ src/components/1-Header.js
@@ -1,1 +1,27 @@
-const Header = () => {};
+//IMAGES
+import logo from "../images/ruLogo.png";
+
+const Header = () => {
+  //HEADER ITEMS
+  const headerItems = ["Home", "Queens", "Seasons", "Favs"];
+
+  //RENDERS
+  // RENDER MENU
+  const RenderMenu = headerItems.map((item, i) => {
+    return (
+      <li key={i}>
+        <a href="">{item}</a>
+      </li>
+    );
+  });
+
+  return (
+    <header>
+      <img src={logo} alt="RuPaul's Drag Race Logo"></img>
+      <nav>
+        <ul>{RenderMenu}</ul>
+      </nav>
+    </header>
+  );
+};
+export default Header;
