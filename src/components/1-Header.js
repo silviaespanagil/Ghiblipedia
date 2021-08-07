02f7e34c852354adb3d533976748ede34a323df0
silviaespanagil <silviaespanagil@gmail.com>
2021-08-07 13:54:58 +0200

    Change headeritem object so it renders path for links

--- src/components/1-Header.js
+++ src/components/1-Header.js
@@ -3,14 +3,21 @@ import logo from "../images/ruLogo.png";
 
 const Header = () => {
   //HEADER ITEMS
-  const headerItems = ["Home", "Queens", "Seasons", "Favs"];
+
+  const headerItems = [
+    { item: "Home", path: "/" },
+    { item: "Queens", path: "/queens" },
+    { item: "Seasons", path: "/seasons" },
+    { item: "Favs", path: "/favorites" },
+  ];
 
   //RENDERS
+
   // RENDER MENU
   const RenderMenu = headerItems.map((item, i) => {
     return (
       <li key={i}>
-        <a href="">{item}</a>
+        <a href={item.path}>{item.item}</a>
       </li>
     );
   });
@@ -19,6 +26,7 @@ const Header = () => {
     <header>
       <img src={logo} alt="RuPaul's Drag Race Logo"></img>
       <nav>
+        {" "}
         <ul>{RenderMenu}</ul>
       </nav>
     </header>
