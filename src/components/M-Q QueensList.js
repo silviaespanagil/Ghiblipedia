edc22425bd2fdda6a0e0560e56908fe2ea1c0dc2
silviaespanagil <silviaespanagil@gmail.com>
2021-08-11 16:33:56 +0200

    Add breakpoints to queenList

--- src/components/M-Q QueensList.js
+++ src/components/M-Q QueensList.js
@@ -9,7 +9,7 @@ const QueensList = (props) => {
     return (
       <li key={favorite.id}>
         <h3>{favorite.name}</h3>
-        <img src={favorite.url} alt={favorite.name} />
+        <img src={favorite.image_url} alt={favorite.name} />
       </li>
     );
   });
@@ -27,19 +27,29 @@ const QueensList = (props) => {
     );
     return (
       <article key={queen.id} className="apiList__card">
-        <button id={index} onClick={() => props.favQueen(queen.id)}>
+        <button
+          apiList__cardid={index}
+          onClick={() => props.favQueen(queen.id)}
+          className="apiList__card--fav"
+        >
           <i
-            className={isFavorite ? "fas fa-lg fa-star" : "far fa-lg fa-star"}
+            className={
+              isFavorite
+                ? "fas fa-lg fa-star apiList__card--fav-icon "
+                : "far fa-lg fa-star apiList__card--fav-icon"
+            }
           ></i>
         </button>
 
         <Link to={`/queens/${queen.id}`}>
-          <img
-            src={queen.image_url}
-            alt={`Portrait of ${queen.name}`}
-            className="apiList__card--img"
-          />
-          <h2 className="apiList__card--name">{queen.name}</h2>
+          <div className="apiList__card--content">
+            <img
+              src={queen.image_url}
+              alt={`Portrait of ${queen.name}`}
+              className="apiList__card--content-img"
+            />
+            <h2 className="apiList__card--content-name">{queen.name}</h2>
+          </div>
         </Link>
       </article>
     );
@@ -47,7 +57,7 @@ const QueensList = (props) => {
   if (favoriteQueens.length !== 0) {
     return (
       <>
-        <section>{allQueens}</section>
+        <section className="queensWrapper">{allQueens}</section>
         <div>
           <h2>My top queens</h2>
           <ul>{favoriteQueens}</ul>
